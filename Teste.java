25a6ac935a1adf31b36ad54e61cfad6d12626c1a
Fátima Pinheiro <30393500+fatimapinheir@users.noreply.github.com>
2021-05-12 17:16:09 -0300

    Create Teste.java

---
+++ Teste.java
@@ -0,0 +1,18 @@
+/**
+ * Teste da disciplina CK0084.
+ * 
+ * @author (Fatima Pinheiro) 
+ * @version (1.0 12/05/2021)
+ */
+public class Helloword extends Saudacao_2{
+//Declaração de variáveis
+private String nome;
+
+//Construtor da classe
+    public Helloword (String n){
+        nome = n;
+        System.out.print(nome);
+    }
+    
+// 
+}
